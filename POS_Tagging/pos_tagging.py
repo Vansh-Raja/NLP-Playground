d485ac44999a4ec71afaf0e76a54604e15faf0c1
UrAvgProGamer <vanshraja32@gmail.com>
2024-02-20 14:49:28 +0530

    todo - spacyPOS

--- POS_Tagging/pos_tagging.py
+++ POS_Tagging/pos_tagging.py
@@ -1,5 +1,6 @@
 import nltk
 from nltk.tokenize import word_tokenize
+import spacy
 
 def nltk_postag(sentence: str = None, words: list = None):
     
@@ -7,4 +8,13 @@ def nltk_postag(sentence: str = None, words: list = None):
         return nltk.pos_tag(words)
     
     return nltk.pos_tag(word_tokenize(sentence))
+
+def spacy_postag(sentence: str):
+    #TODO
+    
+    spacy_model = spacy.load('en_core_web_sm')
+    postag = []
+    
+    for token in spacy_model(sentence):
+        pass
 
